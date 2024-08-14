8dd4916e0c7d4546797c12613ceb495e0f738026
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 19:47:15 -0300

    Chamando o arquivo de layout activity_main a partir da MainActivity.kt

--- app/src/main/java/com/fiap/listadecompras/MainActivity.kt
+++ app/src/main/java/com/fiap/listadecompras/MainActivity.kt
@@ -15,5 +15,7 @@ import com.fiap.listadecompras.ui.theme.ListaDeComprasTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
     }
 }
