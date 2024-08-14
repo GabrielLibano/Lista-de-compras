5be351c29810416a643f9d3d4a6f0b1a5754f4d9
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 20:01:45 -0300

    Criando a classe de adaptador do recyclerView: ItemsAdapter.kt

---
+++ app/src/main/java/com/fiap/listadecompras/ItemsAdapter.kt
@@ -0,0 +1,12 @@
+package com.fiap.listadecompras
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemsAdapter : RecyclerView.Adapter<ItemsAdapter.ItemViewHolder>() {
+
+
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    }
+}
